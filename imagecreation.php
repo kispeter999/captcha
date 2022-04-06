58b3bd40487cfee5bdba873e613f68ff712a1ee6
Meeoro <kispeter999@gmail.com>
2022-04-06 16:52:11 +0200

    fixed font path and adjusted text size on image

--- imagecreation.php
+++ imagecreation.php
@@ -8,14 +8,14 @@ $bg = imagecolorallocate($img, 22, 86, 165);        // background color blue
 $fg = imagecolorallocate($img, 255, 255, 255);      // text color white    
 
 imagefill($img, 0, 0, $bg);                                     // fill the image with the background color
-$bbox = imagettfbbox(30, 0, 'arial.ttf', $captcha);             // get the bounding box of the text
+$bbox = imagettfbbox(25, 0, './arial.ttf', $captcha);             // get the bounding box of the text
 
 $x = ceil((150 - $bbox[4] + $bbox[0])/2);                       // calculate the x position of the text so the text is centered on the image
 $y = ceil((60 - $bbox[5] + $bbox[1])/2);                        // calculate the y position
 
 
 
-imagettftext($img, 30, 0, $x, $y, $fg, 'arial.ttf', $captcha);  // write text on image
+imagettftext($img, 25, 0, $x, $y, $fg, './arial.ttf', $captcha);  // write text on image
 
 
 
